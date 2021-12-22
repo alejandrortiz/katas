fa15c63da1aeac22ed2fc8120f6a174520d35865
Alejandro Ortiz <alejandro.ortiz@avoristravel.com>
2021-12-22 19:36:47 +0100

    feat: Introduced day 16.

---
+++ js/advent_21/exec_16.js
@@ -0,0 +1,73 @@
+/**
+
+Lara Eloft ha encontrado unos restos élficos en una cueva, 
+cerca del Círculo Polar Ártico, a 8 km al norte de Rovaniemi.
+
+Ahora se encuentra descifrando unas misteriosas cartas que 
+contiene información sobre unos números que le puede hacer 
+llegar al próximo objetivo.
+
+Lara tiene un documento que contiene una serie de números que 
+pueden ser usados para descifrarlos:
+
+Símbolo       Valor
+  .             1
+  ,             5
+  :             10
+  ;             50
+  !             100
+
+Lara, además, ha notado una cosa. Los símbolos se restan si 
+están inmediatamente a la izquierda de otro mayor. 😱
+
+Tenemos que crear una función que nos pasa una cadena de texto 
+con símbolos y tenemos que transformarlo al número correcto. 
+¡Ojo! Si encuentras un símbolo que no entendemos, mejor que 
+devolvamos un NaN:
+
+decodeNumbers('...') // 3
+decodeNumbers('.,') // 4 (5 - 1)
+decodeNumbers(',.') // 6 (5 + 1)
+decodeNumbers(',...') // 8 (5 + 3)
+decodeNumbers('.........!') // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+decodeNumbers('.;') // 49 (50 - 1)
+decodeNumbers('..,') // 5 (-1 + 1 + 5)
+decodeNumbers('..,!') // 95 (1 - 1 - 5 + 100)
+decodeNumbers('.;!') // 49 (-1 -50 + 100)
+decodeNumbers('!!!') // 300
+decodeNumbers(';!') // 50
+decodeNumbers(';.W') // NaN
+
+ */
+
+function decodeNumbers(numbers) {
+    const translator = { ".": 1, ",": 5, ":": 10, ";": 50, "!": 100 };
+
+    const result = numbers
+        .split("")
+        .reduce(
+            (accum, number, index) => {
+                if (!translator.hasOwnProperty(number)) return Number.NaN;
+
+                const operation = translator[number] < translator[numbers[index + 1]] ? '-' : '+';
+
+                return `${accum} ${operation} ${translator[number]}`
+            },
+            ''
+        );
+
+    return eval(result);
+}
+
+console.log(decodeNumbers('...')) // 3
+console.log(decodeNumbers('.,')) // 4 (5 - 1)
+console.log(decodeNumbers(',.')) // 6 (5 + 1)
+console.log(decodeNumbers(',...')) // 8 (5 + 3)
+console.log(decodeNumbers('.........!')) // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+console.log(decodeNumbers('.;')) // 49 (50 - 1)
+console.log(decodeNumbers('..,')) // 5 (-1 + 1 + 5)
+console.log(decodeNumbers('..,!')) // 95 (1 - 1 - 5 + 100)
+console.log(decodeNumbers('.;!')) // 49 (-1 -50 + 100)
+console.log(decodeNumbers('!!!')) // 300
+console.log(decodeNumbers(';!')) // 50
+console.log(decodeNumbers(';.W')) // NaN
